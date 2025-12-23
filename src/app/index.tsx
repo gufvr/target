8517e178dae1a3fedfb270354b5eb993901f1657
Gustavo <gustavo.vfavero@gmail.com>
2025-12-23 20:48:58 -0300

    feat: add Target component with navigation back functionality

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { router, Router } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello Expo Router!</Text>
+
+      <Button title='Nova Meta' onPress={() => router.navigate('/target')} />
     </View>
   )
 }
