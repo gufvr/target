1f8bb4632d4d1f55d4826966d632e79f35b0b65e
Gustavo <gustavo.vfavero@gmail.com>
2025-12-24 09:50:51 -0300

    feat: add HomeHeader component with linear gradient background and update dependencies

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,11 @@
-import { View, Text, Button } from 'react-native'
-import { router, Router } from 'expo-router'
+import { View } from 'react-native'
 
-import { fontFamily } from '@/theme/fontFamily'
+import { HomeHeader } from '../components/HomeHeader'
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold }}>Hello Expo Router!</Text>
-
-      <Button title='Nova Meta' onPress={() => router.navigate('/target')} />
-
-      <Button
-        title='Transação'
-        onPress={() => router.navigate('./transaction/765890')}
-      />
-
-      <Button
-        title='Progresso'
-        onPress={() => router.navigate('./in-progress/69')}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   )
 }
---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
+import { styles } from './styles'
+import { colors } from '@/theme/colors'
+
+export function HomeHeader() {
+  return (
+    <LinearGradient
+      colors={[colors.blue[500], colors.blue[800]]}
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+      </View>
+    </LinearGradient>
+  )
+}
