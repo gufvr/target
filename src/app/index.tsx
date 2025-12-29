c2c571bda5b2c0a0279079f9afaf09f755704d8d
Gustavo <gustavo.vfavero@gmail.com>
2025-12-29 18:23:39 -0300

    feat: add Target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
 
-import { HomeHeader } from '../components/HomeHeader'
+import { HomeHeader } from '@/components/HomeHeader'
+import { Target } from '@/components/Target'
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -8,10 +9,21 @@ const summary = {
   output: { label: 'Saidas', value: '-R$ 883,65' },
 }
 
+const targets = [
+  {
+    name: 'Apple Watch',
+    current: 'R$ 580,00',
+    percentage: '50%',
+    target: 'R$ 1.790,00',
+  },
+]
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <Target data={targets[0]} />
     </View>
   )
 }
