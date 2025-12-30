7573f3abab3803e5514931170a4a749c8194be79
Gustavo <gustavo.vfavero@gmail.com>
2025-12-30 12:15:29 -0300

    feat: add Option component for transaction type selection

---
+++ src/components/TransactionType/option.tsx
@@ -0,0 +1,37 @@
+import { ColorValue, Pressable, PressableProps, Text } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { styles } from './styles'
+import { colors } from '@/theme'
+
+type Props = PressableProps & {
+  isSelected: boolean
+  title: string
+  icon: keyof typeof MaterialIcons.glyphMap
+  selectedColor: 'green' | 'red'
+}
+
+export function Option({
+  isSelected,
+  title,
+  icon,
+  selectedColor,
+  ...rest
+}: Props) {
+  return (
+    <Pressable
+      style={[styles.option, isSelected && { backgroundColor: selectedColor }]}
+      {...rest}
+    >
+      <MaterialIcons
+        name={icon}
+        size={24}
+        color={isSelected ? colors.white : colors.gray[500]}
+      />
+
+      <Text
+        style={[styles.title, isSelected && { color: colors.white }]}
+      ></Text>
+    </Pressable>
+  )
+}
